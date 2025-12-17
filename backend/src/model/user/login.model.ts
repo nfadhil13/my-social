255421ec96e1b0b45201bfe84f0023af2c6b4e46
Naufal Fadhil <naufalfadhil12344@gmail.com>
2025-12-17 22:16:18 +0700

    [Auth] Create Login Response

--- backend/src/model/user/login.model.ts
+++ backend/src/model/user/login.model.ts
@@ -1,4 +1,10 @@
 import { z } from 'zod';
 import { AuthValidation } from './auth.validation';
+import { User } from '../../common/prisma/client/client';
 
 export type LoginDto = z.infer<typeof AuthValidation.LOGIN>;
+
+export type LoginResponse = {
+  user: Omit<User, 'password_hash'>;
+  accessToken: string;
+};
